4bc154b8922b59eee1e674b5a520fddd533be1d7
Neelima <neelimaakkiraju2001@gmail.com>
2024-07-11 18:46:12 +0530

    added react code

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-const heading = React.createElement("h1" , {id : "heading"} ,"Hello World From React!");
+const heading = React.createElement("h1" , {} , "Hello World From React");
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
 root.render(heading)
