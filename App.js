f2e04f4af9946261c821637918714097e7351499
Neelima <neelimaakkiraju2001@gmail.com>
2024-07-12 18:00:02 +0530

    new changes done

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
-const heading = React.createElement("h1" , {} , "Hello World From React");
+const parent = React.createElement("div" , {} , 
+    React.createElement("div" , {id : "parent"} , React.createElement("h1" , {} , "I'm h1 tag")),
+    React.createElement("h2" , {id : "child"} , "I'm h1 tag") ,
+    React.createElement("p" , {id : "child"} , "Pragraph created")
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(heading)
+root.render(parent);
