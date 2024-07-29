f747b9045a521b9858f5591d2d080119b2a6719b
Neelima <neelimaakkiraju2001@gmail.com>
2024-07-29 18:19:57 +0530

    new changes

--- App.js
+++ App.js
@@ -3,7 +3,16 @@ import ReactDOM from "react-dom/client"
 
 const heading = React.createElement("h1" , {id: "heading"}, "Hello World From React JS");
 
+const NewComponent2 = () => (
+    <h1>New component</h1>
+)
+const NewComponent = () => (
+    <div>
+         <h1>New Functional component</h1>
+         <NewComponent2/>
+    </div>
+)
 const jsxHeading = <h1>hello world from JSX</h1>
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading) 
+root.render(<NewComponent/>) 
