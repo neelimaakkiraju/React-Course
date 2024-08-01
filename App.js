581d32d799068da507258f43c4c65db08e1da695
Neelima <neelimaakkiraju2001@gmail.com>
2024-08-01 12:12:50 +0530

    added body and header components

--- App.js
+++ App.js
@@ -4,26 +4,59 @@ import ReactDOM from "react-dom/client";
 const HeadComponent = () => {
   return (
     <div className="head-container">
-      <img
-        src="https://png.pngtree.com/template/20191014/ourmid/pngtree-pin-food-delivery-map-location-delivery-logo-concept-image_318151.jpg"
-        alt="Logo"
-        className="logo"
-      />
-      
-      <div className="navbar">
-      <ul>
-        <li>Home</li>
-        <li>Contact us</li>
-        <li>About</li>
-        <li>Cart</li>
-      </ul>
+      <div>
+        <img
+          src="https://png.pngtree.com/template/20191014/ourmid/pngtree-pin-food-delivery-map-location-delivery-logo-concept-image_318151.jpg"
+          alt="Logo"
+          className="logo"
+        />
       </div>
-      
-    
-      
+      <div className="nav-container">
+        <ul>
+          <li>Home</li>
+          <li>Contact us</li>
+          <li>About</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestCard = () => {
+  return (
+    <div>
+      <div className="res-container">
+      <img src="https://cdn.pixabay.com/photo/2024/02/10/00/53/biryani-8563961_1280.jpg" className="card-img"/>
+
+      <h3>Neelima Foods</h3>
+      <h4>South Indian Special</h4>
+      <p>4.5 Star Rating</p>
+      <p>30 Minutes</p>
+    </div>
+    </div>
+  );
+};
+const BodyComponent = () => {
+  return (
+    <div className="body-container">
+      <div className="search-bar">
+        <h3>Search Bar</h3>
+      </div>
+      <div>
+        <RestCard />
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <HeadComponent />
+      <BodyComponent />
     </div>
   );
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadComponent/>);
+root.render(<AppLayout />);
