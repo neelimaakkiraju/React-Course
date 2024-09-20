b752293b20ca6b2898af643415e4e6ff8d4e5090
Neelima <neelimaakkiraju2001@gmail.com>
2024-09-20 09:39:35 +0530

    map,filter functions added

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,10 +2,573 @@ import RestCard from "./RestCard";
 import resObj from "../utils/mockData";
 
 const BodyComponent = () => {
+
+  let resList = [
+    {
+      info: {
+        id: "344287",
+        name: "Varalakshmi Tiffins",
+        cloudinaryImageId:
+          "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/c75ff48c-b373-4733-8126-e3180cbb0fa0_344287.jpg",
+        locality: "Banjara Hills",
+        areaName: "Filmnagar",
+        costForTwo: "₹300 for two",
+        cuisines: ["South Indian"],
+        avgRating: 4.6,
+        veg: true,
+        parentId: "6482",
+        avgRatingString: "4.6",
+        totalRatingsString: "23K+",
+        sla: {
+          deliveryTime: 51,
+          lastMileTravel: 8.2,
+          serviceability: "SERVICEABLE",
+          slaString: "50-55 mins",
+          lastMileTravelString: "8.2 km",
+          iconType: "ICON_TYPE_EMPTY",
+        },
+        availability: {
+          nextCloseTime: "2024-09-18 11:59:00",
+          opened: true,
+        },
+        badges: {
+          imageBadges: [
+            {
+              imageId: "Green%20Dot%20Awards/Best%20In%20Dosa.png",
+              description: "Delivery!",
+            },
+            {
+              imageId: "Rxawards/_CATEGORY-South%20Indian.png",
+              description: "Delivery!",
+            },
+            {
+              imageId: "v1695133679/badges/Pure_Veg111.png",
+              description: "pureveg",
+            },
+          ],
+          textExtendedBadges: [
+            {
+              iconId: "guiltfree/GF_Logo_android_3x",
+              shortDescription: "options available",
+              fontColor: "#7E808C",
+            },
+          ],
+        },
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            imageBased: {
+              badgeObject: [
+                {
+                  attributes: {
+                    description: "Delivery!",
+                    imageId: "Green%20Dot%20Awards/Best%20In%20Dosa.png",
+                  },
+                },
+                {
+                  attributes: {
+                    description: "Delivery!",
+                    imageId: "Rxawards/_CATEGORY-South%20Indian.png",
+                  },
+                },
+                {
+                  attributes: {
+                    description: "pureveg",
+                    imageId: "v1695133679/badges/Pure_Veg111.png",
+                  },
+                },
+              ],
+            },
+            textBased: {},
+            textExtendedBadges: {
+              badgeObject: [
+                {
+                  attributes: {
+                    description: "",
+                    fontColor: "#7E808C",
+                    iconId: "guiltfree/GF_Logo_android_3x",
+                    shortDescription: "options available",
+                  },
+                },
+              ],
+            },
+          },
+        },
+        aggregatedDiscountInfoV3: {
+          header: "₹100 OFF",
+          subHeader: "ABOVE ₹299",
+          discountTag: "FLAT DEAL",
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {},
+            video: {},
+          },
+        },
+        reviewsSummary: {},
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {},
+        externalRatings: {
+          aggregatedRating: {
+            rating: "--",
+          },
+        },
+        ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+      },
+      analytics: {},
+      cta: {
+        link: "https://www.swiggy.com/city/hyderabad/varalakshmi-tiffins-banjara-hills-filmnagar-rest344287",
+        type: "WEBLINK",
+      },
+    },
+    {
+      info: {
+        id: "30253",
+        name: "Agra Sweets Banjara",
+        cloudinaryImageId: "nav4ebkoxrwlladx7a5m",
+        locality: "Utkoor- Mogdumpur road",
+        areaName: "Rambagh Colony",
+        costForTwo: "₹250 for two",
+        cuisines: ["Sweets", "Desserts", "Chaat", "Snacks", "Beverages"],
+        avgRating: 4.4,
+        veg: true,
+        parentId: "34",
+        avgRatingString: "4.4",
+        totalRatingsString: "23K+",
+        sla: {
+          deliveryTime: 28,
+          lastMileTravel: 3,
+          serviceability: "SERVICEABLE",
+          slaString: "25-30 mins",
+          lastMileTravelString: "3.0 km",
+          iconType: "ICON_TYPE_EMPTY",
+        },
+        availability: {
+          nextCloseTime: "2024-09-18 23:00:00",
+          opened: true,
+        },
+        badges: {
+          imageBadges: [
+            {
+              imageId: "v1695133679/badges/Pure_Veg111.png",
+              description: "pureveg",
+            },
+          ],
+          textExtendedBadges: [
+            {
+              iconId: "guiltfree/GF_Logo_android_3x",
+              shortDescription: "options available",
+              fontColor: "#7E808C",
+            },
+          ],
+        },
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            imageBased: {
+              badgeObject: [
+                {
+                  attributes: {
+                    description: "pureveg",
+                    imageId: "v1695133679/badges/Pure_Veg111.png",
+                  },
+                },
+              ],
+            },
+            textBased: {},
+            textExtendedBadges: {
+              badgeObject: [
+                {
+                  attributes: {
+                    description: "",
+                    fontColor: "#7E808C",
+                    iconId: "guiltfree/GF_Logo_android_3x",
+                    shortDescription: "options available",
+                  },
+                },
+              ],
+            },
+          },
+        },
+        aggregatedDiscountInfoV3: {
+          header: "40% OFF",
+          subHeader: "UPTO ₹80",
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {},
+            video: {},
+          },
+        },
+        reviewsSummary: {},
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {},
+        externalRatings: {
+          aggregatedRating: {
+            rating: "--",
+          },
+        },
+        ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+      },
+      analytics: {},
+      cta: {
+        link: "https://www.swiggy.com/city/hyderabad/agra-sweets-banjara-utkoor-mogdumpur-road-rambagh-colony-rest30253",
+        type: "WEBLINK",
+      },
+    },
+    {
+      info: {
+        id: "581700",
+        name: "Cheesecakes By CakeZone",
+        cloudinaryImageId:
+          "RX_THUMBNAIL/IMAGES/VENDOR/2024/8/12/0343ec19-4276-465b-ac38-2329d604b87f_581700.jpg",
+        locality: "SATHYA NARAYAN NAGAR",
+        areaName: "Opp:Mythri Hospital",
+        costForTwo: "₹200 for two",
+        cuisines: [
+          "Desserts",
+          "Bakery",
+          "Beverages",
+          "Bengali",
+          "Ice Cream",
+          "Juices",
+          "Sweets",
+        ],
+        avgRating: 4,
+        veg: true,
+        parentId: "348057",
+        avgRatingString: "4.0",
+        totalRatingsString: "141",
+        sla: {
+          deliveryTime: 34,
+          lastMileTravel: 1.8,
+          serviceability: "SERVICEABLE",
+          slaString: "30-35 mins",
+          lastMileTravelString: "1.8 km",
+          iconType: "ICON_TYPE_EMPTY",
+        },
+        availability: {
+          nextCloseTime: "2024-09-19 00:00:00",
+          opened: true,
+        },
+        badges: {
+          imageBadges: [
+            {
+              imageId: "newg.png",
+              description: "Gourmet",
+            },
+          ],
+        },
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            imageBased: {
+              badgeObject: [
+                {
+                  attributes: {
+                    description: "Gourmet",
+                    imageId: "newg.png",
+                  },
+                },
+              ],
+            },
+            textBased: {},
+            textExtendedBadges: {},
+          },
+        },
+        aggregatedDiscountInfoV3: {
+          header: "60% OFF",
+          subHeader: "UPTO ₹120",
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {},
+            video: {},
+          },
+        },
+        reviewsSummary: {},
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {},
+        externalRatings: {
+          aggregatedRating: {
+            rating: "--",
+          },
+        },
+        ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+      },
+      analytics: {},
+      cta: {
+        link: "https://www.swiggy.com/city/hyderabad/cheesecakes-by-cakezone-sathya-narayan-nagar-opp-mythri-hospital-rest581700",
+        type: "WEBLINK",
+      },
+    },
+    {
+      info: {
+        id: "690934",
+        name: "Ovenfresh Cakes And Desserts",
+        cloudinaryImageId:
+          "RX_THUMBNAIL/IMAGES/VENDOR/2024/8/22/e77f16ff-b7db-407a-bee7-79435e665a57_690934.jpg",
+        locality: "SATHYA NARAYAN NAGAR",
+        areaName: "Attapur",
+        costForTwo: "₹200 for two",
+        cuisines: [
+          "Bakery",
+          "Desserts",
+          "Beverages",
+          "Ice Cream",
+          "Sweets",
+          "Juices",
+          "Bengali",
+          "Pizzas",
+        ],
+        avgRating: 4.3,
+        parentId: "1372",
+        avgRatingString: "4.3",
+        totalRatingsString: "211",
+        sla: {
+          deliveryTime: 32,
+          lastMileTravel: 1.8,
+          serviceability: "SERVICEABLE",
+          slaString: "30-35 mins",
+          lastMileTravelString: "1.8 km",
+          iconType: "ICON_TYPE_EMPTY",
+        },
+        availability: {
+          nextCloseTime: "2024-09-19 00:00:00",
+          opened: true,
+        },
+        badges: {},
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            imageBased: {},
+            textBased: {},
+            textExtendedBadges: {},
+          },
+        },
+        aggregatedDiscountInfoV3: {
+          header: "60% OFF",
+          subHeader: "UPTO ₹120",
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {},
+            video: {},
+          },
+        },
+        reviewsSummary: {},
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {},
+        externalRatings: {
+          aggregatedRating: {
+            rating: "--",
+          },
+        },
+        ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+      },
+      analytics: {},
+      cta: {
+        link: "https://www.swiggy.com/city/hyderabad/ovenfresh-cakes-and-desserts-sathya-narayan-nagar-attapur-rest690934",
+        type: "WEBLINK",
+      },
+    },
+    {
+      info: {
+        id: "12126",
+        name: "Subway",
+        cloudinaryImageId:
+          "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/4/b9c7807f-59bb-4e91-83d4-5048d293f44e_12126.JPG",
+        locality: "Langer Houz -Suncity- Bandlaguda",
+        areaName: "Langar Houz",
+        costForTwo: "₹350 for two",
+        cuisines: ["Fast Food", "Salads", "Snacks", "Desserts", "Beverages"],
+        avgRating: 3.8,
+        parentId: "2",
+        avgRatingString: "3.8",
+        totalRatingsString: "6.5K+",
+        sla: {
+          deliveryTime: 34,
+          lastMileTravel: 3.8,
+          serviceability: "SERVICEABLE",
+          slaString: "30-35 mins",
+          lastMileTravelString: "3.8 km",
+          iconType: "ICON_TYPE_EMPTY",
+        },
+        availability: {
+          nextCloseTime: "2024-09-18 23:59:00",
+          opened: true,
+        },
+        badges: {
+          imageBadges: [
+            {
+              imageId: "Green%20Dot%20Awards/Best%20In%20Veg%20Salad.png",
+              description: "Delivery!",
+            },
+          ],
+          textExtendedBadges: [
+            {
+              iconId: "guiltfree/GF_Logo_android_3x",
+              shortDescription: "options available",
+              fontColor: "#7E808C",
+            },
+          ],
+        },
+        isOpen: true,
+        aggregatedDiscountInfoV2: {},
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            imageBased: {
+              badgeObject: [
+                {
+                  attributes: {
+                    description: "Delivery!",
+                    imageId: "Green%20Dot%20Awards/Best%20In%20Veg%20Salad.png",
+                  },
+                },
+              ],
+            },
+            textBased: {},
+            textExtendedBadges: {
+              badgeObject: [
+                {
+                  attributes: {
+                    description: "",
+                    fontColor: "#7E808C",
+                    iconId: "guiltfree/GF_Logo_android_3x",
+                    shortDescription: "options available",
+                  },
+                },
+              ],
+            },
+          },
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {},
+            video: {},
+          },
+        },
+        reviewsSummary: {},
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {},
+        externalRatings: {
+          aggregatedRating: {
+            rating: "--",
+          },
+        },
+        ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+      },
+      analytics: {},
+      cta: {
+        link: "https://www.swiggy.com/city/hyderabad/subway-langer-houz-suncity-bandlaguda-langar-houz-rest12126",
+        type: "WEBLINK",
+      },
+    },
+    {
+      info: {
+        id: "7203",
+        name: "Taj Mahal-Abids",
+        cloudinaryImageId: "8aa874a03b5bd0804b6781b1fd13f02d",
+        locality: "Abids",
+        areaName: "Abids",
+        costForTwo: "₹400 for two",
+        cuisines: ["South Indian", "North Indian", "Biryani", "Snacks"],
+        avgRating: 4.5,
+        veg: true,
+        parentId: "924",
+        avgRatingString: "4.5",
+        totalRatingsString: "62K+",
+        sla: {
+          deliveryTime: 55,
+          lastMileTravel: 6.3,
+          serviceability: "SERVICEABLE",
+          slaString: "50-55 mins",
+          lastMileTravelString: "6.3 km",
+          iconType: "ICON_TYPE_EMPTY",
+        },
+        availability: {
+          nextCloseTime: "2024-09-18 23:00:00",
+          opened: true,
+        },
+        badges: {
+          imageBadges: [
+            {
+              imageId: "v1695133679/badges/Pure_Veg111.png",
+              description: "pureveg",
+            },
+          ],
+        },
+        isOpen: true,
+        aggregatedDiscountInfoV2: {},
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            imageBased: {
+              badgeObject: [
+                {
+                  attributes: {
+                    description: "pureveg",
+                    imageId: "v1695133679/badges/Pure_Veg111.png",
+                  },
+                },
+              ],
+            },
+            textBased: {},
+            textExtendedBadges: {},
+          },
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {},
+            video: {},
+          },
+        },
+        reviewsSummary: {},
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {},
+        externalRatings: {
+          aggregatedRating: {
+            rating: "4.2",
+            ratingCount: "18K+",
+          },
+          source: "GOOGLE",
+          sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+        },
+        ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+      },
+      analytics: {},
+      cta: {
+        link: "https://www.swiggy.com/city/hyderabad/taj-mahal-abids-abids-rest7203",
+        type: "WEBLINK",
+      },
+    }
+  ]
     return (
       <div className="body-container">
         <div className="search-bar">
-          <h3>Search Bar</h3>
+         <button className="search-btn"
+         onClick={()=> {
+          resList = resList.filter((resta)=> resta.info.avgRating<4);
+          console.log(resList)
+         }}
+          >Top Rated Restaurants</button>
         </div>
         <div className="res-body">
           {
