13c6b83ff5de5efab763c51f69a4b559606bc4f8
Neelima <neelimaakkiraju2001@gmail.com>
2024-11-15 11:21:35 +0530

    counter created using class based components

--- src/components/User.js
+++ src/components/User.js
@@ -3,11 +3,16 @@ import { useState } from "react";
 
 const User = ({name,location,contact}) => {
 
-const [count] = useState([0])
+const [count,setCount] = useState([0])
     return(
         <div>
                <h1>Functional Component</h1>
-               <h1>Count:{count}</h1>
+               <h2>Count:{count}</h2>
+               <button onClick={()=> {
+                setCount(count-1)
+               }}>
+                count decrease
+               </button>
                 <h2>Name:{name}</h2>
                 <h2>Location: {location}</h2>
                 <h2>Contact:{contact}</h2>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,7 +4,8 @@ class UserComponent extends React.Component{
     constructor(props){
         super(props)
       this.state={
-        count: 0
+        count: 0,
+        
       }
     }
 
@@ -12,11 +13,19 @@ class UserComponent extends React.Component{
     render(){
 
         const {name,location,contact} = this.props;
-        const {count} = this.state
+        const {count,count2} = this.state
         return(
             <div className="classComp">
                <h1>Class Component</h1>
                <h2>Count:{count}</h2>
+               <button onClick={()=> {
+                this.setState({
+                  count:this.state.count+1,
+                  
+                })
+
+               }}>count increase</button>
+               
                 <h2>Name:{name}</h2>
                 <h2>Location:{location}</h2>
                 <h2>Contact:{contact}</h2>
