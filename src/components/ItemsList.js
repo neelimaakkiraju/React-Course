2cc404d386a624961ebd140a13a5ea641a9e7695
Neelima <neelimaakkiraju2001@gmail.com>
2024-12-09 16:09:00 +0530

    new changes

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -7,24 +7,26 @@ const ItemsList = ({ items }) => {
       {items.map((item) => (
         <div key={item.card.info.id}>
           <div className="text-start text-[15px] p-3 border-b-[1px] border-gray-200 mx-3 flex justify-between">
-            <div className="m-3 text-md font-bold text-gray-600 space-y-4">
+            <div className="m-3 text-md font-bold text-gray-600 space-y-4 w-9/12">
               <span>{item.card.info.name} - ₹</span>
               <span>
                 {item.card.info.price
                   ? item.card.info.price / 100
                   : item.card.info.defaultPrice / 100}
               </span>
-              <p className="text-[13px] font-light">
-                {item.card.info.description}
-              </p>
+             
+                <p className="text-[13px] font-light text-wrap">
+                  {item.card.info.description}
+                </p>
+              
             </div>
-            <div>
+            <div className="">
               <img
                 src={IMG_URL + item.card.info.imageId}
-                className="w-32 h-32 rounded-md"
+                className="w-32 h-32 mx-auto rounded-md border border-gray-200"
               />
 
-              <div className="bg-white border border-gray-300 rounded-md  px-3 font-semibold text-center">
+              <div className="bg-white border border-black rounded-md  font-semibold text-center">
                 <button>ADD +</button>
               </div>
             </div>
--- src/components/ResCategory.js
+++ src/components/ResCategory.js
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import ItemsList from "./ItemsList";
 
 
 const ResCategory = ({data}) => {
 
+    const [showItems, setShowItems] = useState(false)
+
+    const onClickHandler = ()=> {
+        setShowItems(!showItems)
+    }
+
 console.log(data)
-    return <div className="bg-gray-100 shadow-xl rounded-md">
-    <div className="m-4 px-5 flex justify-between ">
+    return <div className="shadow-sm rounded-md">
+    <div className="m-4 px-5 flex justify-between cursor-pointer" onClick={onClickHandler}>
     <span className="font-bold text-lg">{data.title} ({data.itemCards.length})</span>
     <span>⬇️</span>
     </div>
-    <div><ItemsList items={data.itemCards}/></div>
+    <div>{showItems && <ItemsList items={data.itemCards}/>}</div>
     </div>
 }
 
