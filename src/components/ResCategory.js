2f2afdc65d2e333475318c25caff110bb200d2a6
Neelima <neelimaakkiraju2001@gmail.com>
2024-12-09 11:30:10 +0530

    new changes

--- src/components/ResCategory.js
+++ src/components/ResCategory.js
@@ -3,7 +3,7 @@ const ResCategory = ({data}) => {
 console.log(data)
     return <div className="">
     <div className="bg-gray-200 p-2 flex flex-row gap-3 justify-between rounded-md shadow-xl">
-    <span >{data.title}</span>
+    <span >{data.title} ({data.itemCards.length})</span>
     <span>⬇️</span>
     </div>
     </div>
